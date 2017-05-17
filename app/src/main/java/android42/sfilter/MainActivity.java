a03b98d981cb6cd5f6bb6839692fb6b2f9f7018e
Jackie Wu <wkcn@live.cn>
2017-05-17 10:30:48 +0800

    first commit

---
+++ app/src/main/java/android42/sfilter/MainActivity.java
@@ -0,0 +1,177 @@
+package android42.sfilter;
+
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.os.Bundle;
+import android.os.Environment;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.MotionEvent;
+import android.view.TextureView;
+import android.view.View;
+import android.widget.Toast;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import android.util.Log;
+
+
+public class MainActivity extends AppCompatActivity {
+    private static final String TAG = "MainActivity";
+    private static final int REQUEST_CAMERA_PERMISSION = 101;
+    private SRender renderer;
+    private TextureView textureView;
+    private int filterId = 0;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+				renderer.switchCamera();
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+        Log.i(TAG, "camera permission");
+		// 申请相机权限
+        if (ContextCompat.checkSelfPermission(this,
+                Manifest.permission.CAMERA)
+                != PackageManager.PERMISSION_GRANTED) {
+            // Should we show an explanation?
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.CAMERA)) {
+                Toast.makeText(this, "Camera access is required.", Toast.LENGTH_SHORT).show();
+
+            } else {
+                ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.CAMERA},
+                        REQUEST_CAMERA_PERMISSION);
+            }
+
+        } else {
+            setupCameraPreviewView();
+        }
+    }
+
+	// 申请权限
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String permissions[], int[] grantResults) {
+        switch (requestCode) {
+            case REQUEST_CAMERA_PERMISSION: {
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    setupCameraPreviewView();
+                }
+            }
+        }
+    }
+
+	// 设置相机视图
+    void setupCameraPreviewView() {
+        renderer = new SRender(this);
+        textureView = (TextureView) findViewById(R.id.textureView);
+        assert textureView != null;
+        textureView.setSurfaceTextureListener(renderer);
+        // Show original frame when touch the view
+        textureView.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                switch (event.getAction()) {
+                    case MotionEvent.ACTION_DOWN:
+                        renderer.setFilter(0);
+                        break;
+
+                    case MotionEvent.ACTION_UP:
+                    case MotionEvent.ACTION_CANCEL:
+                        renderer.setFilter(filterId);
+                        break;
+                }
+                return true;
+            }
+        });
+
+        textureView.addOnLayoutChangeListener(new View.OnLayoutChangeListener() {
+            @Override
+            public void onLayoutChange(View v, int left, int top, int right, int bottom, int oldLeft, int oldTop, int oldRight, int oldBottom) {
+                renderer.onSurfaceTextureSizeChanged(null, v.getWidth(), v.getHeight());
+            }
+        });
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    private boolean caputre() {
+        String mPath = genSaveFileName(getTitle().toString() + "_", ".png");
+        File imageFile = new File(mPath);
+        if (imageFile.exists()) {
+            imageFile.delete();
+        }
+
+        // create bitmap screen capture
+        Bitmap bitmap = textureView.getBitmap();
+        OutputStream outputStream = null;
+
+        try {
+            outputStream = new FileOutputStream(imageFile);
+            bitmap.compress(Bitmap.CompressFormat.PNG, 90, outputStream);
+            outputStream.flush();
+            outputStream.close();
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+            return false;
+        } catch (IOException e) {
+            e.printStackTrace();
+            return false;
+        }
+
+        return true;
+    }
+
+    private String genSaveFileName(String prefix, String suffix) {
+        Date date = new Date();
+        SimpleDateFormat dateformat1 = new SimpleDateFormat("yyyyMMdd_hhmmss");
+        String timeString = dateformat1.format(date);
+        String externalPath = Environment.getExternalStorageDirectory().toString();
+        return externalPath + "/" + prefix + timeString + suffix;
+    }
+
+}
---
+++ app/src/main/java/android42/sfilter/SRender.java
@@ -0,0 +1,260 @@
+package android42.sfilter;
+
+/**
+ * Created by wkcn on 5/17/17.
+ */
+
+import android.content.Context;
+import android.graphics.SurfaceTexture;
+import android.hardware.Camera;
+import android.hardware.Camera.Parameters;
+import android.hardware.Camera.Size;
+import android.opengl.GLES11Ext;
+import android.opengl.GLES20;
+import android.util.Log;
+import android.util.Pair;
+import android.util.SparseArray;
+import android.view.TextureView;
+import android.view.Surface;
+
+import java.io.IOException;
+
+import javax.microedition.khronos.egl.EGL10;
+import javax.microedition.khronos.egl.EGLConfig;
+import javax.microedition.khronos.egl.EGLContext;
+import javax.microedition.khronos.egl.EGLDisplay;
+import javax.microedition.khronos.egl.EGLSurface;
+
+import android42.sfilter.filters.CameraFilter;
+import android42.sfilter.filters.OriginalFilter;
+
+import android.util.Log;
+
+// 渲染器
+public class SRender implements Runnable, TextureView.SurfaceTextureListener {
+    private static final String TAG = "SRender";
+    private static final int EGL_OPENGL_ES2_BIT = 4;
+    private static final int EGL_CONTEXT_CLIENT_VERSION = 0x3098;
+    private static final int DRAW_INTERVAL = 1000 / 30;
+
+    private Thread renderThread;
+    private Context context;
+    private SurfaceTexture surfaceTexture;
+    private int gwidth, gheight;
+
+	// EGL
+    private EGLDisplay eglDisplay;
+    private EGLSurface eglSurface;
+    private EGLContext eglContext;
+    private EGL10 egl10;
+
+    private Camera camera = null;
+	private int cameraID = 0;
+
+    private SurfaceTexture cameraSurfaceTexture;
+    private int cameraTextureId;
+    private CameraFilter filter;
+    private int filterID = 0;
+    private SparseArray<CameraFilter> cameraFilterMap = new SparseArray<>();
+
+	public SRender(Context context){
+        this.context = context;
+	}
+	@Override
+	public void run(){
+		// 初始化OpenGL
+		initGL(surfaceTexture);
+		// 加载滤镜
+		cameraFilterMap.append(0, new OriginalFilter(context));
+		setFilter(filterID);
+		cameraTextureId = SGLU.genTexture(GLES11Ext.GL_TEXTURE_EXTERNAL_OES);
+		cameraSurfaceTexture = new SurfaceTexture(cameraTextureId);
+		// 相机视图
+		initCamera();
+        while (!Thread.currentThread().isInterrupted()) {
+            try {
+                if (gwidth < 0 && gheight < 0)
+					// 重新设置画布大小
+                    GLES20.glViewport(0, 0, gwidth = -gwidth, gheight = -gheight);
+
+                GLES20.glClear(GLES20.GL_COLOR_BUFFER_BIT);
+
+				// 更新camera Surface
+                synchronized (this) {
+                    cameraSurfaceTexture.updateTexImage();
+                }
+
+                // Draw camera preview
+                filter.draw(cameraTextureId, gwidth, gheight);
+
+                // Flush
+                GLES20.glFlush();
+                egl10.eglSwapBuffers(eglDisplay, eglSurface);
+
+                Thread.sleep(DRAW_INTERVAL);
+
+            } catch (InterruptedException e) {
+                Thread.currentThread().interrupt();
+            }
+        }
+
+        cameraSurfaceTexture.release();
+		// 退出GL
+        GLES20.glDeleteTextures(1, new int[]{cameraTextureId}, 0);
+
+	}
+
+	// Surface Override
+    @Override
+    public void onSurfaceTextureUpdated(SurfaceTexture surface) {
+    }
+	@Override
+	public void onSurfaceTextureSizeChanged(SurfaceTexture surface, int _width, int _height){
+		gwidth = -_width;
+		gheight = -_height;
+	}
+    @Override
+    public boolean onSurfaceTextureDestroyed(SurfaceTexture surface) {
+		if (camera != null){
+			// 释放摄像机
+			releaseCamera();
+		}
+		// 渲染线程
+		if (renderThread != null & renderThread.isAlive()){
+			renderThread.interrupt();
+		}
+		// 释放滤镜
+		filter.release();
+		return true;
+	}
+	@Override
+	public void onSurfaceTextureAvailable(SurfaceTexture surface, int width, int height) {
+		if (renderThread != null && renderThread.isAlive()) {
+			renderThread.interrupt();
+		}
+		renderThread = new Thread(this);
+
+		surfaceTexture = surface;
+		gwidth = -width;
+		gheight = -height;
+
+		openCamera(cameraID);
+
+		renderThread.start();
+	} 
+
+	public boolean openCamera(int id){
+		if (camera == null){
+			try{
+				camera = Camera.open(id);
+				cameraID = id;
+				initCamera();
+				return true;
+			}catch (RuntimeException e){
+				return false;
+			}
+		}
+		return false;
+	}
+
+    private void initCamera(){
+        Parameters parameters = camera.getParameters();
+        if (parameters.getSupportedFocusModes().contains(
+                Camera.Parameters.FOCUS_MODE_CONTINUOUS_PICTURE)) {
+            parameters.setFocusMode(Parameters.FOCUS_MODE_CONTINUOUS_PICTURE);
+        }
+        parameters.setRotation(90);
+        camera.setParameters(parameters);
+
+		try {
+			camera.setPreviewTexture(cameraSurfaceTexture);
+			camera.startPreview();
+		} catch (IOException ioe) {
+		}
+	}
+
+	public void switchCamera(){
+		Camera.CameraInfo cameraInfo = new Camera.CameraInfo();
+		final int numberOfCameras = Camera.getNumberOfCameras();
+		cameraID = (cameraID + 1) % numberOfCameras;
+		releaseCamera();
+		openCamera(cameraID);
+	}
+
+    public void releaseCamera(){
+        if(camera != null){
+            camera.stopPreview();
+            camera.release();
+            camera = null;
+        }
+    }
+
+	public void setFilter(int id){
+		filterID = id; 
+		filter = cameraFilterMap.get(id);
+		if (filter != null){
+			filter.onAttach();
+		}
+	}
+
+    private void initGL(SurfaceTexture texture) {
+        egl10 = (EGL10) EGLContext.getEGL();
+
+        eglDisplay = egl10.eglGetDisplay(EGL10.EGL_DEFAULT_DISPLAY);
+        if (eglDisplay == EGL10.EGL_NO_DISPLAY) {
+            throw new RuntimeException("eglGetDisplay failed " +
+                    android.opengl.GLUtils.getEGLErrorString(egl10.eglGetError()));
+        }
+
+        int[] version = new int[2];
+        if (!egl10.eglInitialize(eglDisplay, version)) {
+            throw new RuntimeException("eglInitialize failed " +
+                    android.opengl.GLUtils.getEGLErrorString(egl10.eglGetError()));
+        }
+
+        int[] configsCount = new int[1];
+        EGLConfig[] configs = new EGLConfig[1];
+        int[] configSpec = {
+                EGL10.EGL_RENDERABLE_TYPE,
+                EGL_OPENGL_ES2_BIT,
+                EGL10.EGL_RED_SIZE, 8,
+                EGL10.EGL_GREEN_SIZE, 8,
+                EGL10.EGL_BLUE_SIZE, 8,
+                EGL10.EGL_ALPHA_SIZE, 8,
+                EGL10.EGL_DEPTH_SIZE, 0,
+                EGL10.EGL_STENCIL_SIZE, 0,
+                EGL10.EGL_NONE
+        };
+
+        EGLConfig eglConfig = null;
+        if (!egl10.eglChooseConfig(eglDisplay, configSpec, configs, 1, configsCount)) {
+            throw new IllegalArgumentException("eglChooseConfig failed " +
+                    android.opengl.GLUtils.getEGLErrorString(egl10.eglGetError()));
+        } else if (configsCount[0] > 0) {
+            eglConfig = configs[0];
+        }
+        if (eglConfig == null) {
+            throw new RuntimeException("eglConfig not initialized");
+        }
+
+        int[] attrib_list = {EGL_CONTEXT_CLIENT_VERSION, 2, EGL10.EGL_NONE};
+        eglContext = egl10.eglCreateContext(eglDisplay, eglConfig, EGL10.EGL_NO_CONTEXT, attrib_list);
+        eglSurface = egl10.eglCreateWindowSurface(eglDisplay, eglConfig, texture, null);
+
+        if (eglSurface == null || eglSurface == EGL10.EGL_NO_SURFACE) {
+            int error = egl10.eglGetError();
+            if (error == EGL10.EGL_BAD_NATIVE_WINDOW) {
+                Log.e(TAG, "eglCreateWindowSurface returned EGL10.EGL_BAD_NATIVE_WINDOW");
+                return;
+            }
+            throw new RuntimeException("eglCreateWindowSurface failed " +
+                    android.opengl.GLUtils.getEGLErrorString(error));
+        }
+
+        if (!egl10.eglMakeCurrent(eglDisplay, eglSurface, eglSurface, eglContext)) {
+            throw new RuntimeException("eglMakeCurrent failed " +
+                    android.opengl.GLUtils.getEGLErrorString(egl10.eglGetError()));
+        }
+    }
+
+}
